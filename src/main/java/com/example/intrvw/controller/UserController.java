96b3f8c25ddd98999db37ee3138808d3361c3595
Franco Torriani <franco.torriani@wefox.com>
2023-02-09 10:24:59 +0100

    Controller

--- src/main/java/com/example/intrvw/controller/UserController.java
+++ src/main/java/com/example/intrvw/controller/UserController.java
@@ -5,4 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+
+    // Get users
+
+    // Create user
+
+    // Update user
+
+    // Delete user
 }
